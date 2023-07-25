aa97dbe58af6c1684b2fea47fb587598068c0c0e
Ibrahim Maali <maaliibrahim2022@gmail.com>
2023-07-25 10:14:24 +0300

    first commit

---
+++ cypress/support/step_definitions/Base_Steps.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+import { When, Then, Before, After } from "@badeball/cypress-cucumber-preprocessor";
+
+Before(() => {
+    cy.log("Executes before each Scenario/Test.");
+    cy.clearLocalStorage();
+})
+
+Before({ tags: "@smoke"}, () => {
+    cy.log("Executing Smoke Pack");
+})
+
+After(() => {
+    cy.log("Executes after each Scenario/Test.");
+})
